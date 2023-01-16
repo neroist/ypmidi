1fd310c994b74d6024a5dec8f1e2a074745c6f49
Grave <pmpiga1@gmail.com>
2023-01-16 15:03:14 -0500

    Update ypmidi.nim

--- src/ypmidi.nim
+++ src/ypmidi.nim
@@ -1,3 +1,4 @@
+import std/strformat
 import std/json
 import std/os
 
@@ -5,6 +6,7 @@ import paramidib
 import cligen
 import spinny
 import yaml
+import fab
 
 template withSpinny(text: string; s: Spinner; time = false; body: untyped) = 
   let currentSpinner {. inject .} = newSpinny(text, s, time)
@@ -12,25 +14,28 @@ template withSpinny(text: string; s: Spinner; time = false; body: untyped) =
 
   body
 
-  #currentSpinner.success(successMsg)
-
-proc main(file, `out`: string) = 
-  ## Make music with YAML or JSON files
+proc main(file, `out`: string = "") = 
+  ## Make MIDI music with YAML or JSON files
   
   if file.splitFile().ext notin [".json", ".yaml"]:
-    echo file, " is not a Json or Yaml file."
+    bad &"\"{file}\" is not a JSON or YAML file."
     return
   
+  
+  let outfile = if `out` == "": `out`.changeFileExt("wav")
+                else: `out`
 
   let score = if file.splitFile().ext == ".json": readFile file
               else: $loadtoJson(readFile file)[0]
   
   withSpinny("Making Music...", skDots, time=true):
 
-    createDir(`out`.parentDir)
-    saveMusic(`out`, parseJson score)
+    createDir(outfile.parentDir)
+    saveMusic(outfile, parseJson score)
 
     currentSpinner.success("Completed!")
 
+when isMainModule:
+  clCfg.version = "Version v0.1.1"
 
-dispatch main
+  dispatch main, help={"file": "The JSON or YAML file to read from", "out": "Output file for generated audio"}
